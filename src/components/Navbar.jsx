a4b3b71ab8ecba5d8812e8ac77a8f20d008b3d34
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2023-04-10 09:51:10 -0500

    routes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="over__nav">
+            <nav>
+                <div className="nav_in">
+
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
